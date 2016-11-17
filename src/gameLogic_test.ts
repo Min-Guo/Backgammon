f1a5546ea360d43047c2bc5d5f0b0e6072aac94a
Min-Guo <xmguomin@gmail.com>
2016-11-16 17:17:24 -0800

    Logic test workable version

--- src/gameLogic_test.ts
+++ src/gameLogic_test.ts
@@ -79,30 +79,18 @@ describe("In Backgammon", function() {
       isOk: boolean,
       turnIndexBeforeMove: number,
       boardBeforeMove: Board,
-<<<<<<< HEAD
-      turns: ITurnDelta[],
-      boardAfterMove: Board,      
-=======
       boardAfterMove: Board,
       start: number,
       end: number,
       turns: ITurnDelta[],
->>>>>>> Min-Guo
       // originalSteps: Steps,
       // currentStepsBeforeMove: number[],
       // currentStepsAfterMove: number[],
       turnIndexAfterMove: number,
       endMatchScores: number[]): void {
-<<<<<<< HEAD
-    // let moves: IMiniMove[] = [{start: start, end: end}]; 
-    // let turnsAfterMove: ITurnDelta[] = [{originalSteps: originalSteps, currentSteps: currentStepsAfterMove, moves: moves}];
-    let stateTransition: IStateTransition = {     
-=======
       // // let moves: IMiniMove[] = [{start: start, end: end}]; 
       // let turnsAfterMove: ITurnDelta[] = [{originalSteps: originalSteps, currentSteps: currentStepsAfterMove, moves: moves}];
     let stateTransition: IStateTransition = {
-      
->>>>>>> Min-Guo
       turnIndexBeforeMove: turnIndexBeforeMove,
       stateBeforeMove: boardBeforeMove ? {board: boardBeforeMove, delta: null} : null,
       move: {
@@ -115,43 +103,23 @@ describe("In Backgammon", function() {
     expectStateTransition(isBearOffTime, isOk, stateTransition);
   }
 
-<<<<<<< HEAD
-  it("Initial move", function() {
-    expectStateTransition(OK, {
-      turnIndexBeforeMove: BLACK_TURN,
-      stateBeforeMove: null,
-      move: {
-        turnIndexAfterMove: BLACK_TURN,
-        endMatchScores: NO_ONE_WINS,
-        stateAfterMove: {board: INITIAL_BOARD, delta: null}
-      },
-      numberOfPlayers: null
-    });
-  });
-=======
->>>>>>> Min-Guo
+
+//   it("Initial move", function() {
+//     expectStateTransition(OK, {
+//       turnIndexBeforeMove: BLACK_TURN,
+//       stateBeforeMove: null,
+//       move: {
+//         turnIndexAfterMove: BLACK_TURN,
+//         endMatchScores: NO_ONE_WINS,
+//         stateAfterMove: {board: INITIAL_BOARD, delta: null}
+//       },
+//       numberOfPlayers: null
+//     });
+//   });
 
    it("PLacing BLACK on the empty position is legal.", function() {
     let boardBeforeMove: Tower[] = INITIAL_BOARD;
     let boardAfterMove: Tower[] = 
-<<<<<<< HEAD
-        [ new Tower(0, WHITE_TURN, 0),    new Tower(1, BLACK_TURN, 0),
-          new Tower(2, BLACK_TURN, 1),    new Tower(3, NO_ONE_TURN, 0),
-          new Tower(4, NO_ONE_TURN, 0),   new Tower(5, NO_ONE_TURN, 0),
-          new Tower(6, NO_ONE_TURN, 0),   new Tower(7, WHITE_TURN, 5),
-          new Tower(8, NO_ONE_TURN, 0),   new Tower(9, WHITE_TURN, 3),
-          new Tower(10, NO_ONE_TURN, 0),  new Tower(11, NO_ONE_TURN, 0),
-          new Tower(12, NO_ONE_TURN, 0),  new Tower(13, BLACK_TURN, 6),
-          new Tower(14, WHITE_TURN, 5),   new Tower(15, NO_ONE_TURN, 0),
-          new Tower(16, NO_ONE_TURN, 0),  new Tower(17, NO_ONE_TURN, 0),
-          new Tower(18, BLACK_TURN, 3),   new Tower(19, NO_ONE_TURN, 0),
-          new Tower(20, BLACK_TURN, 5),   new Tower(21, NO_ONE_TURN, 0),
-          new Tower(22, NO_ONE_TURN, 0),  new Tower(23, NO_ONE_TURN, 0),
-          new Tower(24, NO_ONE_TURN, 0),  new Tower(25, WHITE_TURN, 2),
-          new Tower(26, WHITE_TURN, 0),   new Tower(27, BLACK_TURN, 0)];
-    let turns: ITurnDelta[] = [{originalSteps: [5, 6], currentSteps: [], moves:[{start: 2, end: 8}, {start: 8, end: 13}]}];
-    expectMove(OK, BLACK_TURN, boardBeforeMove, turns, boardAfterMove, WHITE_TURN, NO_ONE_WINS);
-=======
         [new Tower(0, WHITE_TURN, 0), new Tower(1, BLACK_TURN, 0),
            new Tower(2, BLACK_TURN, 1), new Tower(3, NO_ONE_TURN, 0),
            new Tower(4, NO_ONE_TURN, 0), new Tower(5, NO_ONE_TURN, 0),
@@ -175,7 +143,6 @@ describe("In Backgammon", function() {
          stateAfterMove: {board: boardAfterMove, delta: {turns: turnsAfterMove}}},
          numberOfPlayers: 2
     });
->>>>>>> Min-Guo
   });
 
   it("PLacing BLACK on the position where only one WHITE is legal.", function() {
